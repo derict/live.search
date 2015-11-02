06389e401dedd6b0a26e7989e78fee19472dbaa4
derict <krie.zho@gmail.com>
2015-11-02 14:58:09 +0700

    Update index.php
    adding identifier using 'get' method

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
   <head>
     <title>Live Search Using typeahead (twitter)</title>
     <script src='jquery.min.js'></script>
-    <script src='typeahead2.min.js'></script>
+    <script src='typeahead.min.js'></script>
     <script>
     $(document).ready(function(){
       $('input.typeahead').typeahead({
         name: 'typeahead',
-        remote:'search.php?key=%QUERY',
+        remote:'search.php?col=actress&key=%QUERY',
         limit : 10
       });
     });
@@ -15,8 +15,8 @@
   </head>
   <body>
 	<form action=index.php method='post'>
-     	<input type='text' name='typeahead' class='typeahead tt-query' autocomplete='off' spellcheck='false'>
-		  <input type='submit'>
+     		<input type='text' name='typeahead' class='typeahead tt-query' autocomplete='off' spellcheck='false'>
+		<input type='submit'>
 	</form>
   </body>
 </html>
@@ -25,8 +25,8 @@
 
 include 'connect.php';
 
-$input = $_POST['typeahead'];
-if($input != NULL){
+if(isset($_POST['typeahead'])){
+	$input = $_POST['typeahead'];
 	$sql = "INSERT INTO actress (name) VALUES ('$input')";
 	mysqli_query($conn, $sql);
 }
