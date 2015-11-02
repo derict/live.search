912950fe28aca39735225aacbd2123101c1b822a
derict <krie.zho@gmail.com>
2015-11-02 14:56:31 +0700

    Update search.php
    add col using 'get' method to identifier

--- search.php
+++ search.php
@@ -1,10 +1,13 @@
 <?php
     $key=$_GET['key'];
+	$col=$_GET['col'];
     $array = array();
     $con=mysql_connect("localhost","root","root");
     $db=mysql_select_db("library",$con);
-    $query=mysql_query("select * from actress where name LIKE '%{$key}%'");
-    while($row=mysql_fetch_assoc($query)){
+    $query=mysql_query("select * from $col where name LIKE '%{$key}%'");
+	//echo $query;
+    while($row=mysql_fetch_assoc($query))
+    {
       $array[] = $row['name'];
     }
     echo json_encode($array);
